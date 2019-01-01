a2eea8126bd8adb8ef20c29d6ade34705627c9f9
Hristiyan Dodov <h.dodov@gmail.com>
2019-01-01 20:51:23 +0200

    Change index.js to use NHP's Express handler

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-var prod = process.env.NODE_ENV === 'production';
-var port = process.env.PORT || 8080;
-
+var nhp = require('nhp');
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.set('view engine', 'nhp');
 app.use('/node_modules', express.static('node_modules'));
 app.use(express.static('static'));
 
 app.get('*', function (req, res, next) {
-    if (prod && req.headers['x-forwarded-proto'] == 'http')  {
+    if (req.headers['x-forwarded-proto'] === 'http')  {
         return res.redirect(301, 'https://' + req.headers.host + req.url);
+    } else {
+        next();
     }
-
-    return next();
 });
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
-
-app.get('/demos', function (req, res) {
-    res.render('demos');
-});
-
+app.get('*', nhp.handlers.express);
 app.get('*', function (req, res) {
     res.render('404');
 });

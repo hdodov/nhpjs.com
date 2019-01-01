2011326b7f9dfb1b9da2120218de78f015daca0b
Hristiyan Dodov <h.dodov@gmail.com>
2019-01-01 10:33:54 +0200

    Add http to https redirection

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+var prod = process.env.NODE_ENV === 'production';
 var port = process.env.PORT || 8080;
+
 var express = require('express');
 var app = express();
 
 app.set('view engine', 'nhp');
 app.use('/node_modules', express.static('node_modules'));
 app.use(express.static('static'));
+
+app.get('*', function (req, res, next) {
+    if (prod && req.protocol === 'http')  {
+        res.redirect('https://' + req.headers.host + req.url);
+    } else {
+        next();
+    }
+});
 
 app.get('/', function (req, res) {
     res.render('index');

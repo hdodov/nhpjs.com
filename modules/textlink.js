040031b9734f0cce27603e79ba9bc388ebc0b16f
Hristiyan Dodov <h.dodov@gmail.com>
2019-01-02 10:21:22 +0200

    Basic fixes

--- modules/textlink.js
+++ modules/textlink.js
@@ -1,4 +1,6 @@
 var links = {
+    'Node.js': 'https://nodejs.org/',
+    'npm': 'https://www.npmjs.com/',
     'fontawesome-svg-core': 'https://www.npmjs.com/package/@fortawesome/fontawesome-svg-core',
     'template literal': 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals',
     'Node module': 'https://nodejs.org/api/modules.html',
@@ -9,7 +11,7 @@ var links = {
     'Promise': 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise',
     'axios': 'https://github.com/axios/axios',
     'bacon ipsum': 'https://baconipsum.com/json-api/',
-    'demos': 'https://github.com/hdodov/nhpjs.com/blob/master/site/demo.nhp'
+    'demos template': 'https://github.com/hdodov/nhpjs.com/blob/master/views/demos.nhp'
 };
 
 module.exports = function (text, key) {
